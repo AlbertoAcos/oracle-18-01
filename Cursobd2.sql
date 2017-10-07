f8e720a39769a84dd403b24bee8b77e6f33f078a
AlbertoAcos <jaac2094@gmail.com>
2017-10-07 09:41:36 -0500

    Add files via upload

---
+++ Cursobd2.sql
@@ -0,0 +1,5 @@
+--Clase del 7/10/18
+--constraint =restricciones
+create table almacen (numero_almacen integer, ubicacion_almacen varchar2(80),
+                      constraint  pk_na primary key (numero_almacen)
+                      );
