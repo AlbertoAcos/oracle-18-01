af4cccd6a936b83b1f30d3e94d76d6c8ac40b24b
AlbertoAcos <jaac2094@gmail.com>
2017-10-21 10:37:48 -0500

    Add files via upload

--- cursores.sql
+++ cursores.sql
@@ -64,4 +64,55 @@ end;
 /
 select * from nominas;
 
-
+--Un cursor con update, ademas de funciona como un select
+--realiza actualizaciones de forma masica, es decir, varias actualizaciones
+  create table alumno(id_alumno integer primary key, nombre varchar2(80), semestre integer);
+  
+  insert into alumno values (1,'juan',1);
+  insert into alumno values (2,'paco',1);
+  insert into alumno values (3,'luis',1);
+  insert into alumno values (4,'ramon',1);
+  insert into alumno values (5,'beto',1);
+  
+  select * from alumno;
+  
+  declare 
+  cursor cur_alumnos is select * from alumno for update of semestre;
+  begin 
+  for rec in cur_alumnos loop 
+  update alumno set semestre=2 where current of  cur_alumnos;
+  end loop;
+  end;
+  /
+  
+  select * from alumno;
+  -------------------------------------------------------------
+  create table materia(id_materia integer, cuenta integer,materia varchar2(80),
+                      calificacion float, 
+                      constraint pk_id primary key(id_materia),
+                      constraint fk_cue foreign key(cuenta)
+                      references alumno(id_alumno)
+                      );
+  
+  create sequence sec_materia
+  start with 1
+  increment by 1
+  nomaxvalue;
+  
+  create or replace procedure guardar_materia(mi_id out integer
+                         mi_cuenta in varchar2, mi_cal in float)
+  as 
+  begin
+  select sec_materia.nextval into mi_id from dual;
+  insert into materia values(mi_id,mi_cuenta,mi_cal);
+  end;
+  
+  declare 
+  cursor cur_materia is select * from materia for update of materia;
+  begin 
+  for rec in cur_materia loop 
+  update materia set materia='Base de datos' where current of  cur_materia;
+  end loop;
+  end;
+  /
+  
